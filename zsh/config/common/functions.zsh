c876bed52d28e561af21a0e3ec5c1278aae38e94
Alexander Alesenko <InfHungry@gmail.com>
2024-02-06 19:13:09 +0300

    gitf: add author email

--- zsh/config/common/functions.zsh
+++ zsh/config/common/functions.zsh
@@ -4,7 +4,7 @@ log-in-colors() {
     local red=$(tput bold)$(tput setaf 1)
     local green=$(tput bold)$(tput setaf 2)
     local yellow=$(tput bold)$(tput setaf 3)
-    local blue=$(tput bold)$(tput setaf 4)      
+    local blue=$(tput bold)$(tput setaf 4)
     local off=$(tput sgr0)
 
     # Make sure formatting is reset
@@ -15,9 +15,9 @@ log-in-colors() {
         s/(.*\\bERROR\\b.*)/$red\\0$off/g
       s/(.*\\bSEVERE\\b.*)/$red\\0$off/g
         s/(.*\\bWARN(ING)?\\b.*)/$yellow\\0$off/g
-      s/(.*\\bDEBUG\\b.*)/$blue\\0$off/g        
-      s/\(.*started in.*\)/$green\\0$off/g      
-    "                  
+      s/(.*\\bDEBUG\\b.*)/$blue\\0$off/g
+      s/\(.*started in.*\)/$green\\0$off/g
+    "
     # Make sure formatting is reset
     echo -ne ${off}
 }
@@ -30,7 +30,7 @@ vag() {
 
     if [[ -n $file ]]; then
         vim $file +$line
-    fi    
+    fi
 }
 
 
@@ -44,14 +44,14 @@ tns() {
 
     session_name=$1
     tmux new-session -d -s $session_name
-    tmux switch-client -t "$session_name" &>/dev/null || tmux attach-session -t "$session_name"  
+    tmux switch-client -t "$session_name" &>/dev/null || tmux attach-session -t "$session_name"
 }
 
 tss() {
     local session
     session=$(tmux list-sessions -F "#{session_name}" | \
       fzf --height 40% --reverse --query="$1" --select-1 --exit-0) &&
-    tmux switch-client -t "$session" &>/dev/null || tmux attach-session -t "$session" 
+    tmux switch-client -t "$session" &>/dev/null || tmux attach-session -t "$session"
 }
 
 tks() {
@@ -79,7 +79,7 @@ chstat() {
                 echo $fg_bold[yellow]"hg: "$fg_bold[green]$dir$reset_color
                 hg status $dir | cat
                 echo ""
-            fi    
+            fi
         fi
         if [[ -d "$dir/.git" ]]; then
             ch=$(git -C $dir status --porcelain | wc -c)
@@ -87,28 +87,28 @@ chstat() {
                 echo $fg_bold[yellow]"git: "$fg_bold[green]$dir$reset_color
                 git -C $dir status --short
                 echo ""
-            fi    
-        fi    
-    done    
+            fi
+        fi
+    done
 }
 
 hgf() {
     local rev
-            
+
     rev=$(hg log --color=always --template "{label('custom.rev', rev)} [{label('custom.author', author)}] {splitlines(desc) % '{line} '}\n" "$@" |
           fzf --ansi --no-sort --reverse --tiebreak=index |
           awk -F " " '{print $1}')
 
     if [[ -n $rev ]]; then
         hg diff -c $rev
-    fi    
+    fi
 }
 
 gitf() {
     local commit
-    commit=$(git log --graph --color=always --pretty="format:%C(yellow)%h%C(reset) [%C(#06989A)%an%C(reset)%C(#555753)..%as%C(reset)] %s" "$@" |
+    commit=$(git log --graph --color=always --pretty="format:%C(yellow)%h%C(reset) [%C(#06989A)%an <%ae>%C(reset)%C(#555753)..%as%C(reset)] %s" "$@" |
              fzf --ansi --no-sort --reverse --tiebreak=index --preview-window=down:hidden --preview="(echo {} | grep -o '[a-f0-9]\{7\}') && (echo {} | grep -o '[a-f0-9]\{7\}' | xargs git show --name-status | batcat --color=always)" --bind 'alt-v:toggle-preview' |
-             grep -o '[a-f0-9]\{7\}' | 
+             grep -o '[a-f0-9]\{7\}' |
              head -1)
 
     if [[ -n $commit ]]; then
