dc784885c1d3b5c085d2ea0665e793f467c15327
Alexander Alesenko <InfHungry@gmail.com>
2024-03-13 17:30:27 +0300

    neovide: font antialiasing and hinting config

--- config/nvim/lua/core/neovide.lua
+++ config/nvim/lua/core/neovide.lua
@@ -1,5 +1,5 @@
 if vim.g.neovide then
     vim.g.neovide_refresh_rate = 60
     vim.g.neovide_cursor_vfx_mode = 'railgun'
-    vim.opt.guifont = 'JetBrainsMono Nerd Font:h10'
+    vim.opt.guifont = 'JetBrainsMono Nerd Font:h10:#e-subpixelantialias:#h-slight'
 end
