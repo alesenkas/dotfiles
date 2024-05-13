b9727cb5c754ee17d8664a7d0af345128c6acac4
alesenkas <InfHungry@gmail.com>
2024-05-13 23:38:13 +0300

    neovode: try Zed font

--- config/nvim/lua/core/neovide.lua
+++ config/nvim/lua/core/neovide.lua
@@ -2,4 +2,5 @@ if vim.g.neovide then
     vim.g.neovide_refresh_rate = 60
     vim.g.neovide_cursor_vfx_mode = 'railgun'
     vim.opt.guifont = 'JetBrainsMono Nerd Font:h10:#e-subpixelantialias:#h-slight'
+    -- vim.opt.guifont = 'ZedMono NF Extd:h11:#e-subpixelantialias:#h-slight'
 end
