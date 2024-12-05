192aaa45d9861b548d0b37306986005d42f20de5
Alexander Alesenko <InfHungry@gmail.com>
2024-12-05 13:49:11 +0300

    zsh (gitf): change pretty format

--- zsh/config/common/functions.zsh
+++ zsh/config/common/functions.zsh
@@ -106,7 +106,7 @@ hgf() {
 
 gitf() {
     local commit
-    commit=$(git log --graph --color=always --pretty="format:%C(yellow)%h%C(reset) [%C(#06989A)%an <%ae>%C(reset)%C(#555753)..%as%C(reset)] %s" "$@" |
+    commit=$(git log --graph --color=always --pretty="format:%C(yellow)%h%C(reset) [%C(#06989A)%ae%C(reset)%C(#555753)..%as (%ar)%C(reset)]%C(auto)%d%Creset %s" "$@" |
              fzf --ansi --no-sort --reverse --tiebreak=index --preview-window=down:hidden --preview="(echo {} | grep -o '[a-f0-9]\{7\}') && (echo {} | grep -o '[a-f0-9]\{7\}' | xargs git show --name-status | batcat --color=always)" --bind 'alt-v:toggle-preview' |
              grep -o '[a-f0-9]\{7\}' |
              head -1)
