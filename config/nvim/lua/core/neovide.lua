0ce9e11d405461058a9883bbad0d70f125e92fdc
alesenkas <InfHungry@gmail.com>
2026-03-28 11:57:41 +0300

    refactor(nvim-treesitter): migrate config

--- config/nvim/lua/core/neovide.lua
+++ config/nvim/lua/core/neovide.lua
@@ -2,5 +2,4 @@ if vim.g.neovide then
     vim.g.neovide_refresh_rate = 60
     vim.g.neovide_cursor_vfx_mode = 'railgun'
     vim.opt.guifont = 'JetBrainsMono Nerd Font:h10:#e-subpixelantialias:#h-slight'
-    -- vim.opt.guifont = 'ZedMono NF Extd:h11:#e-subpixelantialias:#h-slight'
 end
