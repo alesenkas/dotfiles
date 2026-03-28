7017beac429d2b272a5583dbe73ab59796ece459
alesenkas <InfHungry@gmail.com>
2026-03-28 11:57:41 +0300

    refactor(nvim-treesitter): migrate config

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,55 +1,13 @@
 return {
     {
         'nvim-treesitter/nvim-treesitter',
-        build = ':TSUpdateSync',
-        event = { 'BufReadPost', 'BufNewFile' },
-        dependencies = {
-            { 'nvim-treesitter/nvim-treesitter-textobjects' }
-        },
+        lazy = false,
+        build = ':TSUpdate',
         config = function()
-            local configs = require('nvim-treesitter.configs')
-
-            configs.setup({
-                ensure_installed = {
+            require('nvim-treesitter').install {
                     'java', 'kotlin', 'lua', 'rust', 'javascript', 'typescript', 'html', 'css',
                     'json', 'yaml', 'vim'
-                },
-                higlight = {
-                    enabled = true,
-                    extended_mode = true,
-                    disable = {},
-                    additional_vim_regex_highlighting = false,
-                },
-                textobjects = {
-                    select = {
-                        enable = true,
-                        keymaps = {
-                            ['af'] = '@function.outer',
-                            ['if'] = '@function.inner',
-                            ['ac'] = '@class.outer',
-                            ['ic'] = '@class.inner',
-                        }
-                    },
-                    swap = {
-                        enable = true,
-                        swap_next = {
-                            ['<leader>L'] = '@parameter.inner',
-                        },
-                        swap_previous = {
-                            ['<leader>H'] = '@parameter.inner',
-                        }
-                    },
-                    move = {
-                        enable = true,
-                        goto_next_start = {
-                            [']]'] = '@function.outer',
-                        },
-                        goto_previous_start = {
-                            ['[['] = '@function.outer',
-                        },
-                    },
-                }
-            })
+            }
         end
     }
 }
